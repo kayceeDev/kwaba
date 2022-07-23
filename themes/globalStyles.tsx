7b0a5eb72998f8a2e8b317d5808fe6a3cb951ec8
kayceedev <Nwobodoe71@gmail.com>
2022-07-24 00:02:04 +0100

    Add global styles
    Signed-off-by: kayceedev <Nwobodoe71@gmail.com>

--- themes/globalStyles.tsx
+++ themes/globalStyles.tsx
@@ -47,6 +47,18 @@ select {
   font: inherit;
 }
 
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+
 /* remove animations for people who've turned them off */
 @media (prefers-reduced-motion: reduce) {
   *,
